0d71d691f20554879e004ea44a3b558fbb1afc30
Diego Najar <dignajar@gmail.com>
2015-06-02 22:41:25 -0300

    English documentation

---
+++ en_EN/databases/site.php
@@ -0,0 +1,19 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{
+    "title": "Bludit",
+    "slogan": "documentation",
+    "description": "",
+    "footer": "Bludit.com - Documentation - \u00a92015",
+    "language": "english",
+    "locale": "en_EN",
+    "timezone": "UTC",
+    "theme": "pure",
+    "adminTheme": "default",
+    "homepage": "getting-started",
+    "postsperpage": "6",
+    "uriPost": "\/post\/",
+    "uriPage": "\/",
+    "uriTag": "\/tag\/",
+    "advancedOptions": "false",
+    "url": "http:\/\/localhost\/bludit\/core\/"
+}
