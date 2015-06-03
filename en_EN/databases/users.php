b9e51140b746832cde7b3db47d422db20bf8a881
Diego Najar <dignajar@gmail.com>
2015-06-02 22:42:41 -0300

    fixed untracked files

--- en_EN/databases/users.php
+++
@@ -1,2 +0,0 @@
-<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
-{"admin":{"firstName":"","lastName":"","twitter":"","role":"admin","password":"9fbba97b6399c92049e2faeb011e096124052792","salt":"GkKLemzwFE","email":"demo","registered":1431994166}}
