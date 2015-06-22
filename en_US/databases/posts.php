45c6db46bc4e2179062ce70bc86b7cd2cb393514
Diego Najar <dignajar@gmail.com>
2015-06-22 00:26:11 -0300

    en_US locale

---
+++ en_US/databases/posts.php
@@ -0,0 +1,12 @@
+<?php defined('BLUDIT') or die('Bludit CMS.'); ?>
+{
+    "prueba": {
+        "description": "",
+        "username": "admin",
+        "status": "published",
+        "tags": "",
+        "allowComments": false,
+        "unixTimeCreated": 1433290407,
+        "unixTimeModified": 0
+    }
+}
